cf633b7b27683524792a803d070a44324a19b298
Tom Henoch <tom.henoch@novadem.com>
2018-03-12 09:21:04 +0100

    fix catch deps

--- deps/catch2.cmake
+++ deps/catch2.cmake
@@ -2,5 +2,5 @@ add_library(catch2 INTERFACE)
 add_library(deps::catch2 ALIAS catch2)
 
 target_include_directories(catch2
-	INTERFACE deps/Catch2/single_include
+	INTERFACE deps/catch2/single_include
 )
