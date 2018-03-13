d0974b0d674fb81f8bb8c9c4cce21b45a7a32803
Tom HENOCH <tom.henoch.dev@gmail.com>
2018-03-13 13:10:32 +0100

    pool rework

--- include/async/pool.h
+++ include/async/pool.h
@@ -15,8 +15,8 @@ class pool
     using event_type = typename queue_type::value_type;
     using size_type = std::size_t;
 
-    pool(size_type number_of_threads = 0) {
-      //add(number_of_threads);
+    pool() {
+
     }
     ~pool() {
       for(auto& thread : threads_) {
@@ -28,12 +28,6 @@ class pool
     pool(const pool&) = delete;
     pool& operator=(const pool&) = delete;
 
-    /*void add(size_type number = 1) {
-      for(size_type i = 0; i < number; ++i) {
-        threads_.push_back(new thread_type(queue_));
-        threads_.back()->start();
-      }
-    }*/
 
     template<typename... Args>
     void add(Args... args) {
